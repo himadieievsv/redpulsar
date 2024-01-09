deb3784ebcfbdc3aafc3cc4b09548c0c59b6c47e
himadieievsv <gimadeevsv@gmail.com>
2024-01-09 19:37:41 +0900

    make some required dependencies transitory

--- redpulsar-jedis/build.gradle.kts
+++ redpulsar-jedis/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
     api("redis.clients:jedis:5.1.0")
-    implementation("org.apache.commons:commons-pool2:2.12.0")
-    implementation(project(mapOf("path" to ":redpulsar-core")))
+    api("org.apache.commons:commons-pool2:2.12.0")
+    api(project(mapOf("path" to ":redpulsar-core")))
 }
