8ae65aa4474fc1d93b69115d46ac45bd5f3ac243
himadieievsv <gimadeevsv@gmail.com>
2024-01-13 19:11:32 +0900

    remove unused argument

--- redpulsar-core/src/main/kotlin/com/himadieiev/redpulsar/core/locks/excecutors/ExecutorAdditions.kt
+++ redpulsar-core/src/main/kotlin/com/himadieiev/redpulsar/core/locks/excecutors/ExecutorAdditions.kt
@@ -5,17 +5,11 @@ import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.yield
 import java.util.Collections
 
-suspend inline fun <T> waitAllJobs(
-    jobs: List<Job>,
-    @Suppress("UNUSED_PARAMETER") results: MutableList<T>,
-) {
+suspend inline fun waitAllJobs(jobs: List<Job>) {
     jobs.joinAll()
 }
 
-suspend inline fun <T> waitMajorityJobs(
-    jobs: List<Job>,
-    results: MutableList<T>,
-) {
+suspend inline fun waitMajorityJobs(jobs: List<Job>) {
     val quorum: Int = jobs.size / 2 + 1
     val results = Collections.synchronizedList(mutableListOf<String>())
     val failed = Collections.synchronizedList(mutableListOf<String>())
