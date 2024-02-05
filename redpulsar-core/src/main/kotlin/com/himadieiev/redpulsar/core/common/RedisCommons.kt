d50e3eb652474df06f22d533dca22825c8c1c6d2
himadieievsv <gimadeevsv@gmail.com>
2024-02-06 02:53:17 +0900

    finalize load script method

--- redpulsar-core/src/main/kotlin/com/himadieiev/redpulsar/core/common/RedisCommons.kt
+++ redpulsar-core/src/main/kotlin/com/himadieiev/redpulsar/core/common/RedisCommons.kt
@@ -2,11 +2,13 @@ package com.himadieiev.redpulsar.core.common
 
 import com.himadieiev.redpulsar.core.locks.abstracts.Backend
 
-const val removeLockScriptPath = "/lua/RemoveLockScript.lua"
-const val setSemaphoreLockScriptPath = "/lua/SetSemaphoreLockScript.lua"
-const val cleanUpExpiredSemaphoreLocksScriptPath = "/lua/CleanUpExpiredSemaphoreLocksScript.lua"
-const val countDownLatchCountScriptPath = "/lua/CountDownLatchCountScript.lua"
+const val removeLockScriptPath = "lua/RemoveLockScript.lua"
+const val setSemaphoreLockScriptPath = "lua/SetSemaphoreLockScript.lua"
+const val cleanUpExpiredSemaphoreLocksScriptPath = "lua/CleanUpExpiredSemaphoreLocksScript.lua"
+const val countDownLatchCountScriptPath = "lua/CountDownLatchCountScript.lua"
 
-fun Backend.loadScript(script: String): String {
-    return this::class.java.getResource("/lua/$script.lua").readText().trimIndent()
+fun Backend.loadScript(scriptPath: String): String {
+    val resourceStream = this::class.java.classLoader.getResourceAsStream(scriptPath)
+        ?: throw IllegalArgumentException("Script $scriptPath not found")
+    return String(resourceStream.readAllBytes()).trimIndent()
 }
