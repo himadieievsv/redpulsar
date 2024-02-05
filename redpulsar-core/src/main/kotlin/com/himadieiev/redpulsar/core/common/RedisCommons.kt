17a76c9f4b0f617ca2b153c2fb06a39541b44f59
himadieievsv <gimadeevsv@gmail.com>
2024-02-06 02:39:45 +0900

    load script with Backend

--- redpulsar-core/src/main/kotlin/com/himadieiev/redpulsar/core/common/RedisCommons.kt
+++ redpulsar-core/src/main/kotlin/com/himadieiev/redpulsar/core/common/RedisCommons.kt
@@ -2,21 +2,11 @@ package com.himadieiev.redpulsar.core.common
 
 import com.himadieiev.redpulsar.core.locks.abstracts.Backend
 
-const val removeLockScript = """
-"""
+const val removeLockScriptPath = "/lua/RemoveLockScript.lua"
+const val setSemaphoreLockScriptPath = "/lua/SetSemaphoreLockScript.lua"
+const val cleanUpExpiredSemaphoreLocksScriptPath = "/lua/CleanUpExpiredSemaphoreLocksScript.lua"
+const val countDownLatchCountScriptPath = "/lua/CountDownLatchCountScript.lua"
 
-const val setSemaphoreLockScript = """
-
-"""
-
-const val cleanUpExpiredSemaphoreLocksScript = """
-
-"""
-
-const val countDownLatchCountScript = """
-
-"""
-
-fun Backend.getScript(script: String): String {
-    return this::class.java.getResource("/lua/$script.lua").readText()
+fun Backend.loadScript(script: String): String {
+    return this::class.java.getResource("/lua/$script.lua").readText().trimIndent()
 }
