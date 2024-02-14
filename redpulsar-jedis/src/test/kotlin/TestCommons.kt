ff4513dabd0a7669a266b70a73031a53103eb474
himadieievsv <gimadeevsv@gmail.com>
2024-02-14 20:30:42 +0900

    Jedis integration tests are using redis cluster

--- redpulsar-jedis/src/test/kotlin/TestCommons.kt
+++ redpulsar-jedis/src/test/kotlin/TestCommons.kt
@@ -2,8 +2,7 @@ import org.apache.commons.pool2.impl.GenericObjectPoolConfig
 import redis.clients.jedis.CommandArguments
 import redis.clients.jedis.Connection
 import redis.clients.jedis.HostAndPort
-import redis.clients.jedis.JedisPooled
-import redis.clients.jedis.UnifiedJedis
+import redis.clients.jedis.JedisCluster
 import redis.clients.jedis.commands.ProtocolCommand
 import redis.clients.jedis.params.SetParams
 import java.time.Duration
@@ -46,7 +45,7 @@ private class TestCommandArguments : CommandArguments(ProtocolCommand { byteArra
 }
 
 /** Integration tests instance initiations */
-fun getInstances(): List<UnifiedJedis> {
+fun getInstances(): List<JedisCluster> {
     val poolConfig =
         GenericObjectPoolConfig<Connection>().apply {
             maxTotal = 64
@@ -57,16 +56,16 @@ fun getInstances(): List<UnifiedJedis> {
         }
 
     val hostPort1 = getHostPort(1)
-    val jedis1 = JedisPooled(poolConfig, hostPort1.host, hostPort1.port, 50)
+    val jedis1 = JedisCluster(hostPort1, 50, poolConfig)
     val hostPort2 = getHostPort(2)
-    val jedis2 = JedisPooled(poolConfig, hostPort2.host, hostPort2.port, 50)
+    val jedis2 = JedisCluster(hostPort2, 50, poolConfig)
     val hostPort3 = getHostPort(3)
-    val jedis3 = JedisPooled(poolConfig, hostPort3.host, hostPort3.port, 50)
+    val jedis3 = JedisCluster(hostPort3, 50, poolConfig)
     return listOf(jedis1, jedis2, jedis3)
 }
 
 private fun getHostPort(number: Int) =
     HostAndPort(
         System.getenv("REDIS_HOST$number") ?: "localhost",
-        (System.getenv("REDIS_PORT$number")?.toInt() ?: (6380 + number)),
+        (System.getenv("REDIS_PORT$number")?.toInt() ?: (7010 + 10 * (number - 1))),
     )
