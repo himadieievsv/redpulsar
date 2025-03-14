fa4b6fdd34c8272852ea5c3ca397c0c96f65524d
himadieievsv <gimadeevsv@gmail.com>
2025-03-14 17:35:45 +0900

    update to cached scripting in jedis

---
+++ redpulsar-jedis/src/main/kotlin/com/himadieiev/redpulsar/jedis/locks/helpers.kt
@@ -0,0 +1,13 @@
+package com.himadieiev.redpulsar.jedis.locks
+
+import com.himadieiev.redpulsar.core.common.LuaScriptEntry
+import redis.clients.jedis.UnifiedJedis
+import redis.clients.jedis.exceptions.JedisNoScriptException
+
+fun UnifiedJedis.evalSha1(luaScriptEntry: LuaScriptEntry, keys: List<String>, args: List<String>): Any? {
+    return try {
+        this.evalsha(luaScriptEntry.sha1, keys, args)
+    } catch (e: JedisNoScriptException) {
+        this.eval(luaScriptEntry.script, keys, args)
+    }
+}
